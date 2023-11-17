31a852ffc26592b35b9978d070a36f38f01eb71e
Yusuf Can Sağlam <yusufcan.sglm@icloud.com>
2023-11-17 21:54:50 +0300

    Initial Commit

---
+++ TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by Yusuf Can Sağlam on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
